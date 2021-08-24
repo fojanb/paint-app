81aef2a6a9bf3d1fb6ab7f69535a9049bab5e754
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 13:24:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ const options = {
   HEIGHT: 500,
   SHAPE: "round",
   COLOR: "#ffffff",
-  "LINE WEIGHT": 5,
+  "LINE WEIGHT":5 ,
 };
 let coord = { x: 0, y: 0 };
 board.width = options.WIDTH;
 board.height = options.HEIGHT;
+
 function draw(event) {
   boardCtx.beginPath();
   boardCtx.lineWidth = options["LINE WEIGHT"];
   boardCtx.lineCap = options.SHAPE;
   boardCtx.strokeStyle = options.COLOR;
+  boardCtx.moveTo(coord.x, coord.y);
   coord.x = event.clientX - board.offsetLeft;
   coord.y = event.clientY - board.offsetTop;
-  boardCtx.moveTo(coord.x, coord.y);
   boardCtx.lineTo(coord.x, coord.y);
- 
   boardCtx.stroke();
 }
 function start() {
