67dc8315752ebc469f4fafc824eda85c2d77f94e
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 11:03:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,25 @@ const options = {
   WIDTH: 900,
   HEIGHT: 500,
 };
+let coord = { x: 0, y: 0 };
 board.width = options.WIDTH;
 board.height = options.HEIGHT;
 
-function draw() {
-  boardCtx.strokeStyle = "#fff";
-  boardCtx.lineWidth = 7;
-  console.log("You are drawing");
+function startDraw(event) {
+    
+    boardCtx.beginPath();
+    boardCtx.lineWidth = 5;
+    boardCtx.lineCap = "round";
+    boardCtx.strokeStyle = "#ffffff";
+    boardCtx.moveTo(coord.x, coord.y);
+    coord.x = event.clientX - board.offsetLeft;
+    coord.y = event.clientY - board.offsetTop;
+    boardCtx.lineTo(coord.x, coord.y);
+    boardCtx.stroke();
 }
-board.addEventListener("click", draw);
+function endDraw() {
+  
+  console.log("End drawing");
+}
+board.addEventListener("mousedown", startDraw);
+board.addEventListener("mouseup", endDraw);
