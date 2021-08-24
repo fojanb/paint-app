30d18d7b9b0031f2b66e8fbc68f9b0ed4b73cab2
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 12:53:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const boardCtx = board.getContext("2d");
 const options = {
   WIDTH: 900,
   HEIGHT: 500,
-  SHAPE : "round",
-  COLOR : "#ffffff",
-  "LINE WEIGHT" : 5,
+  SHAPE: "round",
+  COLOR: "#ffffff",
+  "LINE WEIGHT": 5,
 };
 let coord = { x: 0, y: 0 };
 board.width = options.WIDTH;
@@ -15,19 +15,19 @@ function draw(event) {
   boardCtx.beginPath();
   boardCtx.lineWidth = options["LINE WEIGHT"];
   boardCtx.lineCap = options.SHAPE;
-  boardCtx.strokeStyle = options.COLOR ;
+  boardCtx.strokeStyle = options.COLOR;
   coord.x = event.clientX - board.offsetLeft;
   coord.y = event.clientY - board.offsetTop;
   boardCtx.moveTo(coord.x, coord.y);
   boardCtx.lineTo(coord.x, coord.y);
+ 
   boardCtx.stroke();
 }
 function start() {
   board.addEventListener("mousemove", draw);
 }
 function end() {
-  board.removeEventListener("mousemove", draw)
-  console.log("End drawing");
+  board.removeEventListener("mousemove", draw);
 }
 board.addEventListener("mousedown", start);
 board.addEventListener("mouseup", end);
