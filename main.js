a615023238031838e8be77d740e77d0ceb52b5fb
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 10:28:16 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const wrapper = document.querySelector(".wrapper");
+const board = document.querySelector(".board");
+const options = {
+    WIDTH : 900,
+    HEIGHT : 500,
+}
+board.width = options.WIDTH;
+board.height = options.HEIGHT;
