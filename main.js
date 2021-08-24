f2769405eb1b646aeab0f6998454489b16d096d5
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 11:50:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,20 +8,23 @@ const options = {
 let coord = { x: 0, y: 0 };
 board.width = options.WIDTH;
 board.height = options.HEIGHT;
-
-function startDraw(event) {
+function draw(event) {
   boardCtx.beginPath();
   boardCtx.lineWidth = 5;
   boardCtx.lineCap = "round";
   boardCtx.strokeStyle = "#ffffff";
-  boardCtx.moveTo(coord.x, coord.y);
   coord.x = event.clientX - board.offsetLeft;
   coord.y = event.clientY - board.offsetTop;
+  boardCtx.moveTo(coord.x, coord.y);
   boardCtx.lineTo(coord.x, coord.y);
   boardCtx.stroke();
 }
-function endDraw() {
+function start() {
+  board.addEventListener("mousemove", draw);
+}
+function end() {
+    board.removeEventListener("mousemove", draw)
   console.log("End drawing");
 }
-board.addEventListener("mousedown", startDraw);
-board.addEventListener("mouseup", endDraw);
+board.addEventListener("mousedown", start);
+board.addEventListener("mouseup", end);
