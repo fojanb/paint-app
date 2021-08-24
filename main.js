747e1379f490dab62d664f48517fb88eba6fc65b
Fojan Babaali <fozhanmail@gmail.com>
2021-08-24 10:41:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const wrapper = document.querySelector(".wrapper");
 const board = document.querySelector(".board");
+const boardCtx = board.getContext("2d");
 const options = {
-    WIDTH : 900,
-    HEIGHT : 500,
-}
+  WIDTH: 900,
+  HEIGHT: 500,
+};
 board.width = options.WIDTH;
 board.height = options.HEIGHT;
+
+function draw() {
+  boardCtx.strokeStyle = "#fff";
+  boardCtx.lineWidth = 7;
+  console.log("You are drawing");
+}
+board.addEventListener("click", draw);
