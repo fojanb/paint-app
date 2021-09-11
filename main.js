05bd21b2d6dc1b2c1f3bfdb9e8bdec923a64a170
Fojan Babaali <fozhanmail@gmail.com>
2021-09-10 19:18:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -155,6 +155,47 @@ function visualizer(mediaStreamObj) {
     signalCanvasCtx.stroke();
   }
 }
+const audioPlayer = {
+  duration: document.querySelector("#duration"),
+  current: document.querySelector("#current"),
+  playPause: document.querySelector("#playPause"),
+  song: "audio.mp3",
+  peaks: [],
+};
+let timeCalcultor = (value) => {
+  let second = Math.floor(value % 60);
+  let minute = Math.floor(value / 60);
+  if (second < 10) {
+    second = "0" + second;
+  }
+  return minute + ":" + second;
+};
+// Prepare wavesurfer object
+wavesurfer = WaveSurfer.create({
+  audioContext: {},
+  container: "#wave",
+  waveColor: "#d8f3dc",
+  progressColor: "#06d6a0",
+  height: 48,
+  backend: "MediaElement",
+  scrollParent: false,
+  barWidth : 2,
+  barMinHeight :1,
+});
+for (let i = 0; i < 100; i++) {
+  audioPlayer.peaks.push(Math.random());
+}
+// Load audio
+wavesurfer.load(audioPlayer.song, audioPlayer.peaks);
+audioPlayer.playPause.addEventListener("click", (e) => {
+  wavesurfer.playPause();
+});
+wavesurfer.on("ready", (e) => {
+  audioPlayer.duration.textContent = timeCalcultor(wavesurfer.getDuration(e));
+});
+wavesurfer.on("audioprocess", (e) => {
+  audioPlayer.current.textContent = timeCalcultor(wavesurfer.getCurrentTime(e));
+});
 // >>>------------> Width Scale <------------<<<
 artRoom.widthScale.addEventListener("change", () => {
   ctx.lineWidth = artRoom.widthScale.value;
